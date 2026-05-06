c2b18dd2e94a531885b554ec997635f6193666d9
Simoliva05 <simoneberti05@gmail.com>
2026-05-06 07:49:38 +0200

    Add consistency and publish item routes

--- routes/items.routes.js
+++ routes/items.routes.js
@@ -3,11 +3,30 @@ const router = express.Router();
 
 const { validateObjectIdParam } = require("../middlewares/validateObjectIdParam");
 
-const { createItem, updateItem, listItems, getItem, deleteItem } = require("../controllers/items.controller");
+const {
+  createItem,
+  updateItem,
+  listItems,
+  getItem,
+  checkItemConsistency,
+  publishItem,
+  deleteItem,
+} = require("../controllers/items.controller");
+
 const validateMuseumId = validateObjectIdParam("museumId");
 const validateItemId = validateObjectIdParam("itemId");
 
 router.route("/museums/:museumId/items").all(validateMuseumId).get(listItems).post(createItem);
+
+router
+  .route("/museums/:museumId/items/:itemId/check-consistency")
+  .all(validateMuseumId, validateItemId)
+  .post(checkItemConsistency);
+
+router
+  .route("/museums/:museumId/items/:itemId/publish")
+  .all(validateMuseumId, validateItemId)
+  .post(publishItem);
 
 router.route("/museums/:museumId/items/:itemId").all(validateMuseumId, validateItemId).get(getItem).put(updateItem).patch(updateItem).delete(deleteItem);
 
