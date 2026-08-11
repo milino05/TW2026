c878ba0969e99137807ae7e7eff826449f8e6fd6
Simoliva05 <simoneberti05@gmail.com>
2026-08-11 12:20:29 +0200

    feat: expose visit session learning

---
+++ controllers/visitSessions.controller.js
@@ -0,0 +1,6 @@
+const service = require("../services/visitSession.service");
+async function start(req, res, next) { try { res.status(201).json(await service.startSession({ userId: req.user._id, visitId: req.body.visitId, movementPacePreference: req.body.movementPacePreference })); } catch (error) { next(error); } }
+async function transition(req, res, next) { try { res.json(await service.recordTransition({ sessionId: req.params.sessionId, userId: req.user._id, payload: req.body })); } catch (error) { next(error); } }
+async function stop(req, res, next) { try { res.json(await service.recordStop({ sessionId: req.params.sessionId, userId: req.user._id, payload: req.body })); } catch (error) { next(error); } }
+async function complete(req, res, next) { try { res.json(await service.completeSession({ sessionId: req.params.sessionId, userId: req.user._id })); } catch (error) { next(error); } }
+module.exports = { start, transition, stop, complete };
