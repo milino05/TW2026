9237367be99ab4777731eca2b61ae2aab6191fef
Simoliva05 <simoneberti05@gmail.com>
2026-08-11 14:49:31 +0200

    refactor: add pseudonymous learning contributions

---
+++ models/learningContribution.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const LearningContributionSchema = new Schema(
+  {
+    contributorHash: { type: String, required: true, index: true, select: false },
+    metricType: {
+      type: String,
+      enum: [
+        "population_speed",
+        "population_observation",
+        "museum_movement_factor",
+        "museum_observation_factor",
+        "pace_factor",
+        "connection_residual",
+        "routing_attribute_residual",
+        "item_observation_factor",
+        "item_observation_seconds",
+        "visit_timing_residual",
+        "visit_total_seconds",
+      ],
+      required: true,
+      index: true,
+    },
+    scopeKey: { type: String, required: true, index: true },
+    value: { type: Number, required: true },
+    sampleCount: { type: Number, min: 1, default: 1 },
+    confidence: { type: Number, min: 0, max: 1, default: 0 },
+    firstObservedAt: { type: Date, default: Date.now },
+    lastObservedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true },
+);
+
+LearningContributionSchema.index({ contributorHash: 1, metricType: 1, scopeKey: 1 }, { unique: true });
+LearningContributionSchema.index({ metricType: 1, scopeKey: 1, lastObservedAt: -1 });
+module.exports = mongoose.model("LearningContribution", LearningContributionSchema);
