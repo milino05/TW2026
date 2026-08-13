576cd18ad3431c191f46c13cf8a2382263e69736
Simoliva05 <simoneberti05@gmail.com>
2026-08-13 18:16:44 +0200

    update session controller

--- controllers/visitSessions.controller.js
+++ controllers/visitSessions.controller.js
@@ -1,7 +1,8 @@
 const service=require("../services/visitSession.service"),adaptation=require("../services/planAdaptation.service"),sessionPlans=require("../services/sessionPlan.service");
 async function start(req,res,next){try{res.status(201).json(await service.startSession({userId:req.user._id,visitId:req.body.visitId,movementPacePreference:req.body.movementPacePreference,timeBudgetSeconds:req.body.timeBudgetSeconds}))}catch(error){next(error)}}
 async function transition(req,res,next){try{res.json(await service.recordTransition({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body}))}catch(error){next(error)}}
-async function stop(req,res,next){try{res.json(await service.recordStop({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body}))}catch(error){next(error)}}
+async function contentExperience(req,res,next){try{res.json(await service.recordContentEntryExperience({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body}))}catch(error){next(error)}}
+async function physicalObservation(req,res,next){try{res.json(await service.recordPhysicalTargetObservation({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body}))}catch(error){next(error)}}
 async function interaction(req,res,next){try{res.json(await service.recordInteraction({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body}))}catch(error){next(error)}}
 async function presentationDepth(req,res,next){try{res.json(await service.changePresentationDepth({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body||{}}))}catch(error){next(error)}}
 async function pause(req,res,next){try{res.json(await service.pauseSession({sessionId:req.params.sessionId,userId:req.user._id}))}catch(error){next(error)}}
@@ -12,4 +13,4 @@ async function currentPlan(req,res,next){try{res.json(await sessionPlans.getCurr
 async function proposeChange(req,res,next){try{res.status(201).json(await adaptation.proposePlanChange({userId:req.user._id,sessionId:req.params.sessionId,payload:req.body||{}}))}catch(error){next(error)}}
 async function acceptChange(req,res,next){try{res.json(await adaptation.resolvePlanChangeProposal({userId:req.user._id,proposalId:req.params.proposalId,accept:true}))}catch(error){next(error)}}
 async function rejectChange(req,res,next){try{res.json(await adaptation.resolvePlanChangeProposal({userId:req.user._id,proposalId:req.params.proposalId,accept:false}))}catch(error){next(error)}}
-module.exports={start,transition,stop,interaction,presentationDepth,pause,resume,routeCompleted,complete,currentPlan,proposeChange,acceptChange,rejectChange};
+module.exports={start,transition,contentExperience,physicalObservation,interaction,presentationDepth,pause,resume,routeCompleted,complete,currentPlan,proposeChange,acceptChange,rejectChange};
