6b93894e13c3b1df9cbd9b409ea12c233afbfd84
Simoliva05 <simoneberti05@gmail.com>
2026-08-14 15:54:41 +0200

    expose presentation language adaptation

--- controllers/visitSessions.controller.js
+++ controllers/visitSessions.controller.js
@@ -5,6 +5,7 @@ async function contentExperience(req,res,next){try{res.json(await service.record
 async function physicalObservation(req,res,next){try{res.json(await service.recordPhysicalTargetObservation({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body}))}catch(error){next(error)}}
 async function interaction(req,res,next){try{res.json(await service.recordInteraction({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body}))}catch(error){next(error)}}
 async function presentationDepth(req,res,next){try{res.json(await service.changePresentationDepth({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body||{}}))}catch(error){next(error)}}
+async function presentationLanguage(req,res,next){try{res.json(await service.changePresentationLanguage({sessionId:req.params.sessionId,userId:req.user._id,payload:req.body||{}}))}catch(error){next(error)}}
 async function pause(req,res,next){try{res.json(await service.pauseSession({sessionId:req.params.sessionId,userId:req.user._id}))}catch(error){next(error)}}
 async function resume(req,res,next){try{res.json(await service.resumeSession({sessionId:req.params.sessionId,userId:req.user._id}))}catch(error){next(error)}}
 async function routeCompleted(req,res,next){try{res.json(await service.markRouteCompleted({sessionId:req.params.sessionId,userId:req.user._id}))}catch(error){next(error)}}
@@ -13,4 +14,4 @@ async function currentPlan(req,res,next){try{res.json(await sessionPlans.getCurr
 async function proposeChange(req,res,next){try{res.status(201).json(await adaptation.proposePlanChange({userId:req.user._id,sessionId:req.params.sessionId,payload:req.body||{}}))}catch(error){next(error)}}
 async function acceptChange(req,res,next){try{res.json(await adaptation.resolvePlanChangeProposal({userId:req.user._id,proposalId:req.params.proposalId,accept:true}))}catch(error){next(error)}}
 async function rejectChange(req,res,next){try{res.json(await adaptation.resolvePlanChangeProposal({userId:req.user._id,proposalId:req.params.proposalId,accept:false}))}catch(error){next(error)}}
-module.exports={start,transition,contentExperience,physicalObservation,interaction,presentationDepth,pause,resume,routeCompleted,complete,currentPlan,proposeChange,acceptChange,rejectChange};
+module.exports={start,transition,contentExperience,physicalObservation,interaction,presentationDepth,presentationLanguage,pause,resume,routeCompleted,complete,currentPlan,proposeChange,acceptChange,rejectChange};
