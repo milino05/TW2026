a04d966725a774c019dcfadf06fa84570e1595e2
Simoliva05 <simoneberti05@gmail.com>
2026-08-21 18:52:39 +0200

    refactor: add generated visits v2 routes

---
+++ routes/generatedVisitsV2.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { requireAuth } = require("../middlewares/auth");
+const { validateObjectIdParam } = require("../middlewares/validateObjectIdParam");
+const controller = require("../controllers/generatedVisitsV2.controller");
+
+const validatePlanId = validateObjectIdParam("planId");
+router.post("/v2/generated-plans", requireAuth, controller.generate);
+router.get("/v2/generated-plans/:planId", requireAuth, validatePlanId, controller.get);
+router.post("/v2/generated-plans/:planId/accept", requireAuth, validatePlanId, controller.accept);
+
+module.exports = router;
