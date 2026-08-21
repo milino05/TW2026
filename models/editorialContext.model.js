2762ac9e78e5ded6412a31aad282beeccadbe2ed
Simoliva05 <simoneberti05@gmail.com>
2026-08-21 17:17:02 +0200

    feat: add EditorialContext model

---
+++ models/editorialContext.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const EditorialContextSchema = new Schema({
+  contentSpaceId: { type: Schema.Types.ObjectId, ref: "ContentSpace", required: true, index: true },
+  namespaceId: { type: Schema.Types.ObjectId, ref: "Namespace", required: true, index: true },
+  displayName: { type: String, required: true, trim: true, index: true },
+  shortDescription: { type: String, trim: true, default: null },
+  description: { type: String, trim: true, default: null },
+  lifecycleStatus: { type: String, enum: ["active", "trashed"], default: "active", index: true },
+  trashedAt: { type: Date, default: null },
+  trashedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
+  createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+}, { timestamps: true });
+
+EditorialContextSchema.index({ contentSpaceId: 1, namespaceId: 1 }, { unique: true });
+EditorialContextSchema.index({ contentSpaceId: 1, lifecycleStatus: 1, displayName: 1 });
+EditorialContextSchema.index({ namespaceId: 1, lifecycleStatus: 1, displayName: 1 });
+
+module.exports = mongoose.model("EditorialContext", EditorialContextSchema);
