a9e068a8119cb9428a9a7c38ea03bea6ba405652
Simoliva05 <simoneberti05@gmail.com>
2026-08-21 21:08:11 +0200

    refactor: remove legacy domain architecture

--- routes/navigation.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { requireAuth } = require("../middlewares/auth");
-const { validateObjectIdParam } = require("../middlewares/validateObjectIdParam");
-const controller = require("../controllers/navigation.controller");
-const validateMuseumId = validateObjectIdParam("museumId");
-router.post("/museums/:museumId/navigation/route", requireAuth, validateMuseumId, controller.route);
-router.post("/museums/:museumId/navigation/intent", requireAuth, validateMuseumId, controller.intent);
-module.exports = router;
