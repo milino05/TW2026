f49da8f3a53cb4a6ca4fc0e042709111091c8050
Simoliva05 <simoneberti05@gmail.com>
2026-08-21 17:30:18 +0200

    feat: track working graph and published release

--- models/editorialContext.model.js
+++ models/editorialContext.model.js
@@ -7,6 +7,8 @@ const EditorialContextSchema = new Schema({
   displayName: { type: String, required: true, trim: true, index: true },
   shortDescription: { type: String, trim: true, default: null },
   description: { type: String, trim: true, default: null },
+  workingGraphRevisionId: { type: Schema.Types.ObjectId, ref: "SemanticGraphRevision", default: null },
+  publishedReleaseId: { type: Schema.Types.ObjectId, ref: "EditorialRelease", default: null },
   lifecycleStatus: { type: String, enum: ["active", "trashed"], default: "active", index: true },
   trashedAt: { type: Date, default: null },
   trashedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
@@ -16,5 +18,6 @@ const EditorialContextSchema = new Schema({
 EditorialContextSchema.index({ contentSpaceId: 1, namespaceId: 1 }, { unique: true });
 EditorialContextSchema.index({ contentSpaceId: 1, lifecycleStatus: 1, displayName: 1 });
 EditorialContextSchema.index({ namespaceId: 1, lifecycleStatus: 1, displayName: 1 });
+EditorialContextSchema.index({ publishedReleaseId: 1 });
 
 module.exports = mongoose.model("EditorialContext", EditorialContextSchema);
