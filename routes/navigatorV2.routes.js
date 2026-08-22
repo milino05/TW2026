0a93154e2f16b26f90d4a440be09bf971728a3e5
Milo <milodisalvatore6@gmail.com>
2026-08-22 16:10:01 +0200

    feat: add Navigator projection routes

---
+++ routes/navigatorV2.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { requireAuth } = require("../middlewares/auth");
+const { validateObjectIdParam } = require("../middlewares/validateObjectIdParam");
+const controller = require("../controllers/navigatorV2.controller");
+
+const router = express.Router();
+const visitId = validateObjectIdParam("visitId");
+
+router.use(requireAuth);
+router.get("/v2/navigator/library", controller.library);
+router.get("/v2/navigator/visits/:visitId", visitId, controller.visitDetail);
+
+module.exports = router;
