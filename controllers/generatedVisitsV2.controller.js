b053840e204dbc6b2e7c21ca11e395553e1cb9f1
Milo <milodisalvatore6@gmail.com>
2026-08-22 23:04:09 +0200

    refactor: use GeneratedPlan lifecycle boundary

--- controllers/generatedVisitsV2.controller.js
+++ controllers/generatedVisitsV2.controller.js
@@ -1,5 +1,8 @@
-const generator = require("../services/visitGeneratorV2.service");
 const { generateVisitPlanForUserV2 } = require("../services/generationApplicationV2.service");
+const {
+  getGeneratedPlanForUserV2,
+  acceptGeneratedPlanForUserV2,
+} = require("../services/generatedPlanLifecycleV2.service");
 const { materializeGeneratedPlanV2 } = require("../services/generatedPlanMaterializationV2.service");
 const { projectGeneratedPlanV2 } = require("../services/generatedPlanProjectionV2.service");
 
@@ -11,13 +14,13 @@ async function generate(req, res, next) {
 }
 async function get(req, res, next) {
   try {
-    const plan = await generator.getGeneratedPlanV2({ planId: req.params.planId, userId: req.user._id });
+    const plan = await getGeneratedPlanForUserV2({ planId: req.params.planId, userId: req.user._id });
     res.status(200).json(await projectGeneratedPlanV2(plan));
   } catch (error) { next(error); }
 }
 async function accept(req, res, next) {
   try {
-    const plan = await generator.acceptGeneratedPlanV2({ planId: req.params.planId, userId: req.user._id });
+    const plan = await acceptGeneratedPlanForUserV2({ planId: req.params.planId, userId: req.user._id });
     res.status(200).json(await projectGeneratedPlanV2(plan));
   } catch (error) { next(error); }
 }
