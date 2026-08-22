3e22fff85827d71151a6f92b16af882c9df573d3
Milo <milodisalvatore6@gmail.com>
2026-08-22 16:09:45 +0200

    feat: add Navigator projection controllers

---
+++ controllers/navigatorV2.controller.js
@@ -0,0 +1,18 @@
+const service = require("../services/navigatorVisitV2.service");
+
+async function library(req, res, next) {
+  try {
+    res.status(200).json(await service.listNavigatorLibrary({
+      userId: req.user._id,
+      configuredVenueId: req.query?.configuredVenueId || null,
+    }));
+  } catch (error) { next(error); }
+}
+
+async function visitDetail(req, res, next) {
+  try {
+    res.status(200).json(await service.getNavigatorVisitDetail({ userId: req.user._id, visitId: req.params.visitId }));
+  } catch (error) { next(error); }
+}
+
+module.exports = { library, visitDetail };
