32fbb1b02267eb9c5c4e062ce1bc85fba8ba77b0
Milo <milodisalvatore6@gmail.com>
2026-08-22 21:57:59 +0200

    feat: add Venue target chooser for content authoring

--- routes/marketplaceV2.routes.js
+++ routes/marketplaceV2.routes.js
@@ -2,17 +2,20 @@ const express = require("express");
 const { requireAuth } = require("../middlewares/auth");
 const { validateObjectIdParam } = require("../middlewares/validateObjectIdParam");
 const controller = require("../controllers/marketplaceV2.controller");
+const authoringController = require("../controllers/marketplaceAuthoringV2.controller");
 
 const router = express.Router();
 const listingId = validateObjectIdParam("listingId");
 const offerId = validateObjectIdParam("offerId");
 const itemId = validateObjectIdParam("itemId");
 const namespaceId = validateObjectIdParam("namespaceId");
+const venueId = validateObjectIdParam("venueId");
 const venueTargetId = validateObjectIdParam("venueTargetId");
 
 router.use(requireAuth);
 router.get("/v2/marketplace/catalog", controller.catalog);
 router.get("/v2/marketplace/venue-selector", controller.venueSelector);
+router.get("/v2/marketplace/venues/:venueId/authoring-targets", venueId, authoringController.venueAuthoringTargets);
 router.get("/v2/marketplace/item-authoring/:itemId", itemId, controller.itemAuthoringProjection);
 router.get("/v2/marketplace/namespace-authoring/:namespaceId", namespaceId, controller.namespaceAuthoringControls);
 router.get("/v2/marketplace/venue-targets/:venueTargetId/authoring-context", venueTargetId, controller.venueTargetAuthoringContext);
