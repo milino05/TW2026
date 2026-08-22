bcd3b42e721cfd91e53f51226051f42f7e377a8a
Milo <milodisalvatore6@gmail.com>
2026-08-23 00:27:02 +0200

    feat: expose visit authoring projections

--- controllers/marketplaceAuthoringV2.controller.js
+++ controllers/marketplaceAuthoringV2.controller.js
@@ -1,5 +1,6 @@
 const { listVenueAuthoringTargets } = require("../services/venueAuthoringTargetsV2.service");
 const { getEditorialReleaseComposer } = require("../services/editorialReleaseComposerV2.service");
+const { getVisitAuthoringProjection, searchVisitAuthoringContent } = require("../services/visitAuthoringV2.service");
 
 async function venueAuthoringTargets(req, res, next) {
   try {
@@ -16,4 +17,43 @@ async function editorialReleaseComposer(req, res, next) {
   } catch (error) { next(error); }
 }
 
-module.exports = { venueAuthoringTargets, editorialReleaseComposer };
+async function newVisitAuthoring(req, res, next) {
+  try {
+    res.status(200).json(await getVisitAuthoringProjection({
+      actorUserId: req.user._id,
+      principalType: req.query?.principalType || "user",
+      principalId: req.query?.principalId || req.user._id,
+    }));
+  } catch (error) { next(error); }
+}
+
+async function visitAuthoring(req, res, next) {
+  try {
+    res.status(200).json(await getVisitAuthoringProjection({
+      actorUserId: req.user._id,
+      visitId: req.params.visitId,
+    }));
+  } catch (error) { next(error); }
+}
+
+async function visitAuthoringContent(req, res, next) {
+  try {
+    res.status(200).json(await searchVisitAuthoringContent({
+      actorUserId: req.user._id,
+      editorialReleaseId: req.params.editorialReleaseId,
+      principalType: req.query?.principalType || "user",
+      principalId: req.query?.principalId || req.user._id,
+      queryText: req.query?.q || "",
+      page: req.query?.page,
+      limit: req.query?.limit,
+    }));
+  } catch (error) { next(error); }
+}
+
+module.exports = {
+  venueAuthoringTargets,
+  editorialReleaseComposer,
+  newVisitAuthoring,
+  visitAuthoring,
+  visitAuthoringContent,
+};
