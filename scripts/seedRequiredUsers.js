6bb742f8e7eadb7d4332ea63246cf456611d8610
Milo <milodisalvatore6@gmail.com>
2026-08-23 00:06:24 +0200

    refactor: reuse exam seed user setup

--- scripts/seedRequiredUsers.js
+++ scripts/seedRequiredUsers.js
@@ -2,44 +2,21 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const mongoose = require("mongoose");
-const User = require("../models/user");
-const { hashPassword } = require("../services/auth.service");
+const { ensureRequiredUsers, REQUIRED_USERNAMES } = require("./examDatasetV2");
 
-const REQUIRED_USERNAMES = ["autore1", "autore2", "visitatore1", "visitatore2"];
-const DEFAULT_PASSWORD = "12345678";
-
-async function seedRequiredUsers() {
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI mancante");
-  }
-
+async function main() {
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI mancante");
   await mongoose.connect(process.env.MONGO_URI);
-
-  for (const username of REQUIRED_USERNAMES) {
-    const passwordHash = await hashPassword(DEFAULT_PASSWORD);
-    const existingUser = await User.findOne({ username });
-
-    if (existingUser) {
-      existingUser.passwordHash = passwordHash;
-      existingUser.status = "active";
-      await existingUser.save();
-      console.log(`Aggiornato utente ${username}`);
-    } else {
-      await User.create({
-        username,
-        passwordHash,
-        organizationMemberships: [],
-        status: "active",
-      });
-      console.log(`Creato utente ${username}`);
-    }
+  try {
+    await ensureRequiredUsers();
+    for (const username of REQUIRED_USERNAMES) console.log(`Account richiesto pronto: ${username}`);
+  } finally {
+    await mongoose.disconnect();
   }
 }
 
-seedRequiredUsers()
-  .then(() => mongoose.disconnect())
-  .catch(async (error) => {
-    console.error(error);
-    await mongoose.disconnect().catch(() => {});
-    process.exit(1);
-  });
+main().catch(async (error) => {
+  console.error(error);
+  await mongoose.disconnect().catch(() => {});
+  process.exit(1);
+});
