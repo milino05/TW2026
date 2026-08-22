c9f77047cb1c0ba50bdda63aba23c3b5ca1bea2e
Milo <milodisalvatore6@gmail.com>
2026-08-22 16:12:49 +0200

    feat: bootstrap Navigator auth session

--- clients/navigator/src/main.ts
+++ clients/navigator/src/main.ts
@@ -2,14 +2,20 @@ import { createApp } from "vue";
 import { createPinia, setActivePinia } from "pinia";
 import App from "./ui/App.vue";
 import { router } from "./application/router";
-import { useConfiguredVenueStore } from "./application/stores";
+import { useAuthStore, useConfiguredVenueStore } from "./application/stores";
 import { loadNavigatorStaticConfig } from "./domain/navigatorStaticConfig";
+import { authRepository } from "./infrastructure/http/authRepository";
 
 async function bootstrap() {
   const pinia = createPinia();
   setActivePinia(pinia);
 
-  const config = await loadNavigatorStaticConfig();
+  const [config] = await Promise.all([
+    loadNavigatorStaticConfig(),
+    authRepository.me()
+      .then((response) => useAuthStore().setUser(response.user))
+      .catch(() => useAuthStore().clear()),
+  ]);
   useConfiguredVenueStore().bootstrap(config);
 
   createApp(App)
