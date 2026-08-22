0f67a9adfb1ece895ad2294f3a1d92e84a071a3b
Milo <milodisalvatore6@gmail.com>
2026-08-22 23:20:58 +0200

    feat: route generation subject search

--- routes/navigatorV2.routes.js
+++ routes/navigatorV2.routes.js
@@ -10,6 +10,7 @@ router.use(requireAuth);
 router.get("/v2/navigator/library", controller.library);
 router.get("/v2/navigator/sessions", controller.resumableSessions);
 router.get("/v2/navigator/generation-options", controller.generationOptionsProjection);
+router.post("/v2/navigator/generation-subjects/search", controller.generationSubjectOptions);
 router.get("/v2/navigator/visits/:visitId", visitId, controller.visitDetail);
 
 module.exports = router;
