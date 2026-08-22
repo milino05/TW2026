ad6e8c73b36d6a375c5dfa49ceb3aae539c3c2eb
Milo <milodisalvatore6@gmail.com>
2026-08-22 21:59:59 +0200

    feat: expose EditorialRelease composer projection

--- controllers/marketplaceAuthoringV2.controller.js
+++ controllers/marketplaceAuthoringV2.controller.js
@@ -1,4 +1,5 @@
 const { listVenueAuthoringTargets } = require("../services/venueAuthoringTargetsV2.service");
+const { getEditorialReleaseComposer } = require("../services/editorialReleaseComposerV2.service");
 
 async function venueAuthoringTargets(req, res, next) {
   try {
@@ -6,4 +7,13 @@ async function venueAuthoringTargets(req, res, next) {
   } catch (error) { next(error); }
 }
 
-module.exports = { venueAuthoringTargets };
+async function editorialReleaseComposer(req, res, next) {
+  try {
+    res.status(200).json(await getEditorialReleaseComposer({
+      editorialContextId: req.params.editorialContextId,
+      actorUserId: req.user._id,
+    }));
+  } catch (error) { next(error); }
+}
+
+module.exports = { venueAuthoringTargets, editorialReleaseComposer };
