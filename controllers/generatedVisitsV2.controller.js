a85d59c5ba30eb98620dffe5025b3ccaa078ca28
Milo <milodisalvatore6@gmail.com>
2026-08-22 22:54:29 +0200

    refactor: project GeneratedPlan responses for Navigator

--- controllers/generatedVisitsV2.controller.js
+++ controllers/generatedVisitsV2.controller.js
@@ -1,21 +1,27 @@
 const generator = require("../services/visitGeneratorV2.service");
 const { assertGenerationRequestAccess } = require("../services/generationAccess.service");
 const { materializeGeneratedPlanV2 } = require("../services/generatedPlanMaterializationV2.service");
+const { projectGeneratedPlanV2 } = require("../services/generatedPlanProjectionV2.service");
 
 async function generate(req, res, next) {
   try {
     const request = req.body || {};
     await assertGenerationRequestAccess({ request, actorUserId: req.user._id });
-    res.status(201).json(await generator.generateVisitPlanV2({ userId: req.user._id, request }));
+    const plan = await generator.generateVisitPlanV2({ userId: req.user._id, request });
+    res.status(201).json(await projectGeneratedPlanV2(plan));
   } catch (error) { next(error); }
 }
 async function get(req, res, next) {
-  try { res.status(200).json(await generator.getGeneratedPlanV2({ planId: req.params.planId, userId: req.user._id })); }
-  catch (error) { next(error); }
+  try {
+    const plan = await generator.getGeneratedPlanV2({ planId: req.params.planId, userId: req.user._id });
+    res.status(200).json(await projectGeneratedPlanV2(plan));
+  } catch (error) { next(error); }
 }
 async function accept(req, res, next) {
-  try { res.status(200).json(await generator.acceptGeneratedPlanV2({ planId: req.params.planId, userId: req.user._id })); }
-  catch (error) { next(error); }
+  try {
+    const plan = await generator.acceptGeneratedPlanV2({ planId: req.params.planId, userId: req.user._id });
+    res.status(200).json(await projectGeneratedPlanV2(plan));
+  } catch (error) { next(error); }
 }
 async function materialize(req, res, next) {
   try {
