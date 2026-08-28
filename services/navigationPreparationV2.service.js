5aa766096a9be125198b77cb6a4f3aa76f3e3dc5
Simoliva05 <simoneberti05@gmail.com>
2026-08-28 14:55:54 +0200

    fix(routing): retain local profile selections during preparation

--- services/navigationPreparationV2.service.js
+++ services/navigationPreparationV2.service.js
@@ -1,5 +1,6 @@
 const { materializeSessionPhysicalPlan } = require("./physicalExecutionV2.service");
 const { normalizeRoutingRequirements } = require("./routingPreferenceV2.service");
+const { normalizeRoutingProfileSelections } = require("./routingProfileSelectionV2.service");
 
 function normalizeMovementPace(value) {
   const number = Number(value);
@@ -9,6 +10,7 @@ function normalizeMovementPace(value) {
 async function resolveNavigationPreparation({ sourceAnchors = [], sourceLegHints = new Map(), navigation = {} } = {}) {
   const normalizedNavigation = {
     movementPacePreference: normalizeMovementPace(navigation.movementPacePreference),
+    routingProfileSelections: normalizeRoutingProfileSelections(navigation.routingProfileSelections || [], { field: "navigation.routingProfileSelections" }),
     requirements: normalizeRoutingRequirements(navigation.requirements, { field: "navigation.requirements" }),
   };
   return materializeSessionPhysicalPlan({
