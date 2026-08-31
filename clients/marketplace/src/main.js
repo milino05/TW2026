19409477824502059ded74ef328a629e1f53a664
Simoliva05 <simoneberti05@gmail.com>
2026-08-31 11:44:00 +0200

    Load shared legacy feedback surface migration

--- clients/marketplace/src/main.js
+++ clients/marketplace/src/main.js
@@ -1,4 +1,5 @@
 import "./ui/feedback-primitives.js";
 import "./ui/vocabulary-editor-shell.js";
 import "./ui/transient-feedback-adapter.js";
+import "./ui/legacy-feedback-surface-adapter.js";
 import "./ui/app-shell.js";
