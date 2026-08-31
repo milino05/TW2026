77e8b54143264dde055028ab95b3a64016b74aa9
Simoliva05 <simoneberti05@gmail.com>
2026-08-31 17:27:55 +0200

    Activate shared guided tour adapter

--- clients/marketplace/src/main.js
+++ clients/marketplace/src/main.js
@@ -10,4 +10,5 @@ import "./ui/media-viewer-adapter.js";
 import "./ui/revision-workflow-adapter.js";
 import "./ui/authoring-stepper-adapter.js";
 import "./ui/visit-reorder-adapter.js";
+import "./ui/guided-tour-adapter.js";
 import "./ui/app-shell.js";
