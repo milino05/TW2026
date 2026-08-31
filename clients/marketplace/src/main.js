726bc0450f3219c020778cd60239263188de9629
Simoliva05 <simoneberti05@gmail.com>
2026-08-31 16:21:47 +0200

    Bootstrap shared interaction primitives

--- clients/marketplace/src/main.js
+++ clients/marketplace/src/main.js
@@ -1,5 +1,10 @@
 import "./ui/feedback-primitives.js";
+import "./ui/form-field.js";
+import "./ui/action-menu.js";
+import "./ui/media-field.js";
+import "./ui/revision-workflow-controls.js";
 import "./ui/vocabulary-editor-shell.js";
 import "./ui/transient-feedback-adapter.js";
 import "./ui/legacy-feedback-surface-adapter.js";
+import "./ui/authoring-stepper-adapter.js";
 import "./ui/app-shell.js";
