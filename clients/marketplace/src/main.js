43412ee34f1e99e679fa6855d5db9714ce98bec9
Simoliva05 <simoneberti05@gmail.com>
2026-08-31 11:38:37 +0200

    Use shared transient feedback migration adapter

--- clients/marketplace/src/main.js
+++ clients/marketplace/src/main.js
@@ -1,4 +1,4 @@
 import "./ui/feedback-primitives.js";
 import "./ui/vocabulary-editor-shell.js";
-import "./ui/vocabulary-editor-feedback.js";
+import "./ui/transient-feedback-adapter.js";
 import "./ui/app-shell.js";
