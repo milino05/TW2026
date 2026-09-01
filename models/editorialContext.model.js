f387403a51543452f40a71e0b783725a7c81ae34
Simoliva05 <simoneberti05@gmail.com>
2026-09-01 19:30:14 +0200

    refactor: allow multiple collections per editorial space ruleset

--- models/editorialContext.model.js
+++ models/editorialContext.model.js
@@ -8,6 +8,7 @@ const EditorialContextSchema = new Schema({
   shortDescription: { type: String, trim: true, default: null },
   description: { type: String, trim: true, default: null },
   workingGraphRevisionId: { type: Schema.Types.ObjectId, ref: "SemanticGraphRevision", default: null },
+  activeReviewRevisionId: { type: Schema.Types.ObjectId, ref: "EditorialContextRevision", default: null, index: true },
   publishedReleaseId: { type: Schema.Types.ObjectId, ref: "EditorialRelease", default: null },
   lifecycleStatus: { type: String, enum: ["active", "trashed"], default: "active", index: true },
   trashedAt: { type: Date, default: null },
@@ -15,7 +16,7 @@ const EditorialContextSchema = new Schema({
   createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
 }, { timestamps: true });
 
-EditorialContextSchema.index({ contentSpaceId: 1, namespaceId: 1 }, { unique: true });
+EditorialContextSchema.index({ contentSpaceId: 1, namespaceId: 1, lifecycleStatus: 1 });
 EditorialContextSchema.index({ contentSpaceId: 1, lifecycleStatus: 1, displayName: 1 });
 EditorialContextSchema.index({ namespaceId: 1, lifecycleStatus: 1, displayName: 1 });
 EditorialContextSchema.index({ publishedReleaseId: 1 });
