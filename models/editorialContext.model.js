7389ae28880c1cdbe1f6582e4d2d6dcf7911665a
Simoliva05 <simoneberti05@gmail.com>
2026-09-02 10:48:23 +0200

    Reference reusable semantic graph from editorial contexts

--- models/editorialContext.model.js
+++ models/editorialContext.model.js
@@ -4,10 +4,10 @@ const { Schema } = mongoose;
 const EditorialContextSchema = new Schema({
   contentSpaceId: { type: Schema.Types.ObjectId, ref: "ContentSpace", required: true, index: true },
   namespaceId: { type: Schema.Types.ObjectId, ref: "Namespace", required: true, index: true },
+  semanticGraphId: { type: Schema.Types.ObjectId, ref: "SemanticGraph", required: true, index: true },
   displayName: { type: String, required: true, trim: true, index: true },
   shortDescription: { type: String, trim: true, default: null },
   description: { type: String, trim: true, default: null },
-  workingGraphRevisionId: { type: Schema.Types.ObjectId, ref: "SemanticGraphRevision", default: null },
   workingVersion: { type: Number, min: 0, default: 0 },
   activeReviewRevisionId: { type: Schema.Types.ObjectId, ref: "EditorialContextRevision", default: null, index: true },
   publishedReleaseId: { type: Schema.Types.ObjectId, ref: "EditorialRelease", default: null },
@@ -17,12 +17,9 @@ const EditorialContextSchema = new Schema({
   createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
 }, { timestamps: true });
 
-EditorialContextSchema.index(
-  { contentSpaceId: 1, namespaceId: 1 },
-  { unique: true, partialFilterExpression: { lifecycleStatus: "active" } },
-);
 EditorialContextSchema.index({ contentSpaceId: 1, lifecycleStatus: 1, displayName: 1 });
 EditorialContextSchema.index({ namespaceId: 1, lifecycleStatus: 1, displayName: 1 });
+EditorialContextSchema.index({ semanticGraphId: 1, lifecycleStatus: 1, displayName: 1 });
 EditorialContextSchema.index({ publishedReleaseId: 1 });
 
 module.exports = mongoose.model("EditorialContext", EditorialContextSchema);
