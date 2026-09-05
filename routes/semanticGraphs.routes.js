8e9f8cfae092484085a8b4d4f3c3b3bb1a7bbcd4
Simoliva05 <simoneberti05@gmail.com>
2026-09-05 19:28:48 +0200

    feat: route standalone graph authoring projection

--- routes/semanticGraphs.routes.js
+++ routes/semanticGraphs.routes.js
@@ -14,17 +14,18 @@ router.route("/semantic-graphs")
   .get(controller.list)
   .post(controller.create);
 
-router.route("/semantic-graphs/:semanticGraphId")
-  .all(semanticGraphId)
-  .get(controller.get)
-  .patch(controller.update)
-  .delete(controller.trash);
-
+router.get("/semantic-graphs/:semanticGraphId/authoring", semanticGraphId, controller.authoring);
 router.post("/semantic-graphs/:semanticGraphId/restore", semanticGraphId, controller.restore);
 router.post("/semantic-graphs/:semanticGraphId/fork", semanticGraphId, controller.fork);
 router.get("/semantic-graphs/:semanticGraphId/snapshot", semanticGraphId, controller.getSnapshot);
 router.get("/semantic-graphs/:semanticGraphId/neighborhood", semanticGraphId, controller.getNeighborhood);
 router.get("/semantic-graphs/:semanticGraphId/subjects", semanticGraphId, controller.listSubjects);
+
+router.route("/semantic-graphs/:semanticGraphId")
+  .all(semanticGraphId)
+  .get(controller.get)
+  .patch(controller.update)
+  .delete(controller.trash);
 
 router.route("/semantic-graphs/:semanticGraphId/subjects/:subjectId")
   .all(semanticGraphId, subjectId)
