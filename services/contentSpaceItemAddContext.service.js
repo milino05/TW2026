d6ad4e1c2f5e7d3501589a52900ce9b4a8820d0b
Simoliva05 <simoneberti05@gmail.com>
2026-09-07 09:14:18 +0200

    Compose content space quick add with marketplace acquisition

---
+++ services/contentSpaceItemAddContext.service.js
@@ -0,0 +1,41 @@
+const contentSpaceService = require("./contentSpace.service");
+const contentSpaceItemDetailService = require("./contentSpaceItemDetail.service");
+const { resolveOrganizationAuthority } = require("./organizationAuthorization.service");
+const { listMarketplaceForkOptionsForSubject } = require("./marketplaceSubjectForkOptions.service");
+
+async function canAcquireForPrincipal(contentSpace, actorUserId) {
+  if (contentSpace.ownerType === "user") return true;
+  const authority = await resolveOrganizationAuthority({
+    userId: actorUserId,
+    organizationId: contentSpace.ownerId,
+  });
+  return new Set(authority?.effectivePermissions || []).has("marketplace.acquire");
+}
+
+async function getItemAddContext({ contentSpaceId, subjectId, actorUserId }) {
+  const baseContext = await contentSpaceItemDetailService.getItemAddContext({
+    contentSpaceId,
+    subjectId,
+    actorUserId,
+  });
+  const contentSpace = await contentSpaceService.findContentSpaceOrFail({ contentSpaceId });
+  const canAcquire = await canAcquireForPrincipal(contentSpace, actorUserId);
+  const marketplaceOptions = (baseContext.ownedItems || []).length
+    ? []
+    : await listMarketplaceForkOptionsForSubject({
+      subjectId,
+      ownerType: contentSpace.ownerType,
+      ownerId: contentSpace.ownerId,
+    });
+
+  return {
+    ...baseContext,
+    marketplaceOptions,
+    availableOperations: {
+      ...baseContext.availableOperations,
+      canAcquireAndForkMarketplaceItem: Boolean(baseContext.availableOperations?.canCreateItem && canAcquire),
+    },
+  };
+}
+
+module.exports = { getItemAddContext };
