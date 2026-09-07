ee3caa76a27b87e61628741eb4c122250afabc11
Simoliva05 <simoneberti05@gmail.com>
2026-09-07 14:03:55 +0200

    Remove obsolete collection external import controller flow

--- controllers/marketplaceAuthoringV2.controller.js
+++ controllers/marketplaceAuthoringV2.controller.js
@@ -5,10 +5,6 @@ const {
   getEditorialStudioProjection,
 } = require("../services/editorialStudioV2.service");
 const { listEditorialCollectionAvailableItems } = require("../services/editorialCollectionAvailableItems.service");
-const {
-  searchExternalEditorialCandidates,
-  importExternalEditorialCandidate,
-} = require("../services/editorialContextExternalContent.service");
 const { listEditorialRelationChoices } = require("../services/editorialRelationLauncherV2.service");
 const { createEditorialStudioCollection, listReusableSemanticGraphs } = require("../services/editorialStudioCreationV2.service");
 const { getVisitAuthoringProjection, searchVisitAuthoringContent, searchVisitAuthoringCandidates } = require("../services/visitAuthoringV2.service");
@@ -84,28 +80,6 @@ async function editorialStudioCandidates(req, res, next) {
   } catch (error) { next(error); }
 }
 
-async function editorialExternalCandidates(req, res, next) {
-  try {
-    res.status(200).json(await searchExternalEditorialCandidates({
-      editorialContextId: req.params.editorialContextId,
-      actorUserId: req.user._id,
-      query: req.query?.q || "",
-      page: req.query?.page,
-      limit: req.query?.limit,
-    }));
-  } catch (error) { next(error); }
-}
-
-async function importEditorialExternalCandidate(req, res, next) {
-  try {
-    res.status(201).json(await importExternalEditorialCandidate({
-      editorialContextId: req.params.editorialContextId,
-      itemEditionId: req.body?.itemEditionId,
-      actorUserId: req.user._id,
-    }));
-  } catch (error) { next(error); }
-}
-
 async function createEditorialCollection(req, res, next) {
   try { res.status(201).json(await createEditorialStudioCollection({ payload: req.body || {}, actorUserId: req.user._id })); }
   catch (error) { next(error); }
@@ -163,8 +137,6 @@ module.exports = {
   reusableSemanticGraphs,
   editorialStudio,
   editorialStudioCandidates,
-  editorialExternalCandidates,
-  importEditorialExternalCandidate,
   createEditorialCollection,
   newVisitAuthoring,
   visitAuthoring,
