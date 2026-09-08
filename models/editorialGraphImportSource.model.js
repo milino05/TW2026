55af00c3491e297b55e850fe3369b7b9316913ea
Simoliva05 <simoneberti05@gmail.com>
2026-09-08 22:00:31 +0200

    Add pinned graph import source model

---
+++ models/editorialGraphImportSource.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const EditorialGraphImportSourceSchema = new Schema({
+  editorialContextId: { type: Schema.Types.ObjectId, ref: "EditorialContext", required: true, index: true, immutable: true },
+  targetSemanticGraphId: { type: Schema.Types.ObjectId, ref: "SemanticGraph", required: true, index: true, immutable: true },
+  sourceSemanticGraphId: { type: Schema.Types.ObjectId, ref: "SemanticGraph", required: true, index: true, immutable: true },
+  sourceGraphRevisionId: { type: Schema.Types.ObjectId, ref: "SemanticGraphRevision", required: true, index: true, immutable: true },
+  createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true, immutable: true },
+}, { timestamps: true, collection: "editorial_graph_import_sources" });
+
+EditorialGraphImportSourceSchema.index(
+  { editorialContextId: 1, sourceGraphRevisionId: 1 },
+  { unique: true },
+);
+EditorialGraphImportSourceSchema.index({ targetSemanticGraphId: 1, createdAt: -1 });
+
+module.exports = mongoose.model("EditorialGraphImportSource", EditorialGraphImportSourceSchema);
