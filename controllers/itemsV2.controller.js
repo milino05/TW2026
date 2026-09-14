03860ba78c51600a1734ed1dc63e9a450cf6467c
Milo <milodisalvatore6@gmail.com>
2026-09-14 18:56:08 +0200

    feat(items): expose recognition media update

--- controllers/itemsV2.controller.js
+++ controllers/itemsV2.controller.js
@@ -1,11 +1,13 @@
 const service = require("../services/itemV2.service");
 const instantiation = require("../services/itemInstantiationV2.service");
 const itemCreation = require("../services/itemCreationV2.service");
+const itemRecognitionMedia = require("../services/itemRecognitionMedia.service");
 const authoring = require("../services/itemAuthoringV2.service");
 const mediaUpload = require("../services/itemMediaUpload.service");
 async function create(req, res, next) { try { res.status(201).json(await itemCreation.createItem({ payload: req.body || {}, actorUserId: req.user._id })); } catch (error) { next(error); } }
 async function list(req, res, next) { try { res.status(200).json(await service.listItems(req.query || {})); } catch (error) { next(error); } }
 async function get(req, res, next) { try { res.status(200).json(await service.getItem({ itemId: req.params.itemId })); } catch (error) { next(error); } }
+async function updateRecognitionMedia(req, res, next) { try { res.status(200).json(await itemRecognitionMedia.updateItemRecognitionMedia({ itemId: req.params.itemId, payload: req.body || {}, actorUserId: req.user._id })); } catch (error) { next(error); } }
 async function createEdition(req, res, next) { try { res.status(201).json(await service.createEdition({ itemId: req.params.itemId, payload: req.body || {}, actorUserId: req.user._id })); } catch (error) { next(error); } }
 async function updateEdition(req, res, next) { try { res.status(200).json(await service.updateEdition({ editionId: req.params.editionId, payload: req.body || {}, actorUserId: req.user._id })); } catch (error) { next(error); } }
 async function checkEdition(req, res, next) { try { res.status(200).json(await authoring.checkEditionConsistency({ editionId: req.params.editionId, actorUserId: req.user._id })); } catch (error) { next(error); } }
@@ -19,6 +21,7 @@ module.exports = {
   create,
   list,
   get,
+  updateRecognitionMedia,
   createEdition,
   updateEdition,
   checkEdition,
