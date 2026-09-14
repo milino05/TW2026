ff1211aee055611cda2d31f5dd32190cb56d202c
Simoliva05 <simoneberti05@gmail.com>
2026-09-14 11:29:40 +0200

    Restrict persistent navigation preferences to canonical needs

--- services/userPreference.service.js
+++ services/userPreference.service.js
@@ -10,7 +10,7 @@ const policy = require("../config/adaptivePolicy");
 const { contributorHash } = require("./contributorIdentity.service");
 const { removeUserLearningV2 } = require("./learningV2.service");
 const { getActiveUserOrFail } = require("./userAuthorization.service");
-const { normalizeRoutingRequirements } = require("./routingPreferenceV2.service");
+const { normalizePersonalNavigationRequirements } = require("./navigationNeedPreference.service");
 
 function unit(value, field) {
   const number = Number(value);
@@ -34,7 +34,7 @@ function normalizeNavigationPreference(payload = {}) {
     : unit(payload.movementPacePreference, "movementPacePreference");
   return {
     movementPacePreference,
-    requirements: normalizeRoutingRequirements(payload.requirements, { field: "requirements", semanticOnly: true }),
+    requirements: normalizePersonalNavigationRequirements(payload.requirements, { field: "requirements" }),
   };
 }
 
