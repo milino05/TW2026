e6d08bf858e4043a7f5ec7d62773d9161afb5120
Simoliva05 <simoneberti05@gmail.com>
2026-09-14 20:37:40 +0200

    Add demo layout snapshot command

---
+++ scripts/snapshotDemoVenueLayouts.js
@@ -0,0 +1,22 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
+const mongoose = require("mongoose");
+const { exportDemoVenueLayoutSnapshots } = require("./demoVenueLayoutSnapshots");
+
+async function main() {
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI mancante");
+  await mongoose.connect(process.env.MONGO_URI);
+  try {
+    const result = await exportDemoVenueLayoutSnapshots();
+    console.log(JSON.stringify({ status: "ok", ...result }, null, 2));
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+
+main().catch(async (error) => {
+  console.error(error);
+  await mongoose.disconnect().catch(() => {});
+  process.exit(1);
+});
