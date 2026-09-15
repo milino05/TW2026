8f4d25aa690d031901ccb0befebcbec4b1e77bba
Simoliva05 <simoneberti05@gmail.com>
2026-09-15 10:25:24 +0200

    feat: expose navigator venue config management

---
+++ controllers/navigatorVenueConfig.controller.js
@@ -0,0 +1,46 @@
+const service = require("../services/navigatorVenueConfig.service");
+
+async function getVenueConfig(req, res, next) {
+  try {
+    const projection = await service.getVenueNavigatorConfig({ venueId: req.params.venueId, actorUserId: req.user._id });
+    res.status(200).json(projection);
+  } catch (error) { next(error); }
+}
+
+async function updateVenueConfig(req, res, next) {
+  try {
+    const config = await service.updateVenueNavigatorConfig({ venueId: req.params.venueId, actorUserId: req.user._id, payload: req.body || {} });
+    res.status(200).json({ config });
+  } catch (error) { next(error); }
+}
+
+async function uploadVenueAsset(req, res, next) {
+  try {
+    const asset = await service.uploadVenueNavigatorAsset({ venueId: req.params.venueId, actorUserId: req.user._id, payload: req.body || {} });
+    res.status(201).json({ asset });
+  } catch (error) { next(error); }
+}
+
+async function copyVenueConfig(req, res, next) {
+  try {
+    const config = await service.copyVenueNavigatorConfig({
+      venueId: req.params.venueId,
+      sourceVenueId: req.body?.sourceVenueId,
+      actorUserId: req.user._id,
+    });
+    res.status(200).json({ config });
+  } catch (error) { next(error); }
+}
+
+async function navigatorAsset(req, res, next) {
+  try {
+    const asset = await service.getNavigatorAsset(req.params.assetId);
+    res.set("Content-Type", asset.mimeType);
+    res.set("Content-Length", String(asset.byteLength));
+    res.set("Cache-Control", "public, max-age=2592000, immutable");
+    res.set("X-Content-Type-Options", "nosniff");
+    res.status(200).send(asset.data);
+  } catch (error) { next(error); }
+}
+
+module.exports = { getVenueConfig, updateVenueConfig, uploadVenueAsset, copyVenueConfig, navigatorAsset };
